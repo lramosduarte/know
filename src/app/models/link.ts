4988a5c71cb13f31d342a7dfb1adef9437f946dd
Leonardo Ramos Duarte <lramosduarte@gmail.com>
2019-03-21 21:53:51 -0300

    Ajusta model de Link

--- src/app/models/link.ts
+++ src/app/models/link.ts
@@ -1,7 +1,22 @@
 export class Link {
-    url: string;
-    name?: string;
-    dateRead?: Date;
-    dateAdd: Date;
-    favorite?: boolean = false;
+  url: string;
+  name?: string;
+  dateRead?: Date;
+  dateAdd: Date;
+  favorite: boolean;
+
+  constructor(
+      url: string,
+      dateAdd: Date = new Date(),
+      favorite: boolean = false,
+      name: string = null,
+      dateRead: Date = null,
+    ) {
+    this.url = url;
+    this.dateAdd = dateAdd;
+    this.favorite = favorite;
+    this.name = name;
+    this.dateRead = dateRead;
+  }
+
 }
