0eeb8ac1c120ea165b7e3b0b0d7d23bd2850f743
Leonardo Ramos Duarte <lramosduarte@gmail.com>
2019-03-23 16:43:26 -0300

    Adiciona twitter bootstrap
     - Lib que implementa o bootstrap twiter ao angular.
     - Ajusta config do webpack para usar o css do bootstrap na build
     - Altera o tipo default de arquivo de sass para scss
       * Eu não sabia que o sass era um formato antigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,8 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
