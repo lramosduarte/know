3a1b1457abf0ad90af240c9c6724e7c142a40f2c
Leonardo Ramos Duarte <lramosduarte@gmail.com>
2019-04-10 21:05:47 -0300

    Cria e configura um env para dev
    Agora é possível iniciar o ng serve em modo de dev

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDqy5uvCOjM6LwEtgVz2TI1swwjZ-Vjkdo',
+    authDomain: 'know-person-777dd.firebaseapp.com',
+    databaseURL: 'https://know-person-777dd.firebaseio.com',
+    projectId: 'know-person-777dd',
+    storageBucket: 'know-person-777dd.appspot.com',
+    messagingSenderId: "674242616813",
+  }
+};
